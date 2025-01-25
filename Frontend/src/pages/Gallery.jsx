83cb4d65caa5b5aa1c67597f1d2f84543eb3448b
joshankumar6299 <joshankumar6299@gmail.com>
2025-01-25 23:16:30 +0530

    Add Gallery page component for showcasing images

---
+++ Frontend/src/pages/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold">Gallery</h1>
+      <p>Check out some awesome pictures here!</p>
+      {/* Add your gallery content or images here */}
+    </div>
+  );
+}
+
+export default Gallery;
