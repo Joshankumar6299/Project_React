c1a3db69554b53bc491a226d5f8214ffadf0d08a
joshankumar6299 <joshankumar6299@gmail.com>
2025-01-25 23:16:35 +0530

    Add Home page component for welcoming users

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+      <p>This is the homepage of your website!</p>
+    </div>
+  );
+}
+
+export default Home;
