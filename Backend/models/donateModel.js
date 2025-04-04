84a42cccbd27b689e43b847b9b8efae8b2f673f6
Satish Das <dasskumar9801@gmail.com>
2025-04-04 10:56:14 +0530

    Add donateModel.js to define the schema for food donation in the backend

---
+++ Backend/models/donateModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const donateSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    foodType: {
+        type: String,
+        required: true,
+        enum: ['veg', 'non-veg'],
+    },
+    fullAddress: {
+        type: String,
+        required: true,
+    },
+    foodQuantity: {
+        type: String,
+        required: true,
+    },
+    
+})
+
+module.exports = mongoose.model('donate', donateSchema);
