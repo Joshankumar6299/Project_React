2441fd2f2cdf2afb2c0ff615f09ac54337ce85e8
Satish Das <dasskumar9801@gmail.com>
2025-04-04 10:55:29 +0530

    Set up Express middleware and define routes for admin, user, and donation functionalities in the backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+
+
 const connectDB = require('./database/db.js');
 require('dotenv').config();
 connectDB();
+
+app.use(express.json());
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+
+const adminRoutes = require('./routes/adminRoutes.js');
+const userRoutes = require('./routes/userRoutes.js');
+const donateRoutes = require('./routes/donateRoute.js');
+
+app.use('/admin', adminRoutes);
+app.use('/user', userRoutes);
+app.use('/donate', donateRoutes);
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
