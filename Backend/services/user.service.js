691c7717d8812e17d9f8c7d7e8641b29b6687b06
Satish Das <dasskumar9801@gmail.com>
2025-04-04 10:56:44 +0530

    Add user service for registration and login functionalities in the backend

---
+++ Backend/services/user.service.js
@@ -0,0 +1,61 @@
+const bcrypt = require("bcrypt");
+const ApiError = require("../utils/errorHandler.js")
+const User = require("../models/userModel.js");
+
+const registerService = async (data) => {
+    try {
+
+        const existingUser = await User.findOne({ email: data.email});
+        console.log(existingUser);
+        
+        if(existingUser){
+            throw new ApiError(409, "User already exists with this email")
+        }
+        const existingPhone = await User.findOne({phone : data.phone});
+        console.log(existingPhone);
+        
+        if(existingPhone){
+            throw new ApiError(409, "User with this phone already exists")
+        }
+
+        const newUser = await User.create({
+            fullname: data.fullname,
+            password: data.password,
+            email: data.email,
+            phone: data.phone
+        });
+
+        newUser.password = undefined; 
+
+        return newUser;
+    } catch (error) {
+        console.error("Error registering user:", error);
+        throw error;
+    }
+};
+
+const loginService = async (data) => {
+    try {
+        const user = await User.findOne({ email: data.email });
+
+        if (!user) {
+            throw new ApiError(400, "User not found");
+        }
+
+        const isMatch = await bcrypt.compare(data.password, user.password);
+        if (!isMatch) {
+            throw new ApiError(401, "Invalid credentials");
+        }
+
+        return user
+
+
+    } catch (error) {
+        throw error;
+    }
+};
+
+module.exports = {
+    registerService,
+    loginService
+}
